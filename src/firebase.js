3f317534eea80ba07b6fdeccea3325a71043678d
jhonnyer512 <kakaroto639@outlook.com>
2025-05-16 22:57:31 -0500

    7

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase (reemplaza con tus datos reales)
 const firebaseConfig = {
-apiKey: "TU_API_KEY",
-authDomain: "TU_PROYECTO.firebaseapp.com",
-projectId: "TU_PROYECTO",
-storageBucket: "TU_PROYECTO.appspot.com",
-messagingSenderId: "TU_SENDER_ID",
-appId: "TU_APP_ID"
+apiKey: "AIzaSyB-onT7XQmGzf-j16OszKICE9Xm8XC_Ioo",
+  authDomain: "project-a18b2.firebaseapp.com",
+  databaseURL: "https://project-a18b2-default-rtdb.firebaseio.com",
+  projectId: "project-a18b2",
+  storageBucket: "project-a18b2.firebasestorage.app",
+  messagingSenderId: "833529947041",
+  appId: "1:833529947041:web:1055cdf35c63403e29a1e0",
+  measurementId: "G-XSYDRWELLP"
 };
 
 // Inicializar Firebase y Firestore
